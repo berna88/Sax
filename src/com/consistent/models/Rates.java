f7f45b7cbc7a06f12c33f3de43063b7267783007
Bernardo <bhernandez@consistent.com.mx>
2019-01-02 17:42:25 -0600

    Creacion de rates

---
+++ src/com/consistent/models/Rates.java
@@ -0,0 +1,30 @@
+package com.consistent.models;
+
+public class Rates {
+	private String code;
+	private String name;
+	private String keyword;
+	
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getKeyword() {
+		return keyword;
+	}
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	
+	
+
+}
